836bdb10e1dfee7879d886ce49536a19560c7c2e
fsalles2022 <fabiolfs761@gmail.com>
2022-12-29 19:31:24 -0300

    Deletando e Rcriando a Tabela Lesson e fazendo a migrate

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -5,7 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Lessons extends Model
+class Lesson extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'video'];
 }
