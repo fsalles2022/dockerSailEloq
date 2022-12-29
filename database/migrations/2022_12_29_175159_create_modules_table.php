59019e2852c6caabe3f025d2f4dfcd604864fd55
fsalles2022 <fabiolfs761@gmail.com>
2022-12-29 19:16:10 -0300

    Criado os campos e os relacionamentos das tabelas Lessons, Courses e Modules

--- database/migrations/2022_12_29_175159_create_modules_table.php
+++ database/migrations/2022_12_29_175159_create_modules_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('modules', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('course_id')->constrained('courses');
+            $table->string('name');
             $table->timestamps();
         });
     }
--- database/migrations/2022_12_29_175215_create_lessons_table.php
+++ database/migrations/2022_12_29_175215_create_lessons_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('lessons', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('module_id')->constrained('modules');
+            $table->string('name');
+            $table->string('video');
             $table->timestamps();
         });
     }
